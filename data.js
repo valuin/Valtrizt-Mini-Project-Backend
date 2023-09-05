bb2824fd175cadff2b3d93ad9a4dbfa0cd7c8dcd
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-09-05 17:36:55 +0700

    init commit for project

---
+++ data.js
@@ -0,0 +1,9 @@
+// data.js
+const libraryData = [
+    { title: "Buku 1", author: "Penulis 1" },
+    { title: "Buku 2", author: "Penulis 2" },
+    { title: "Buku 3", author: "Penulis 3" },
+    // Tambahkan data buku lainnya sesuai kebutuhan
+];
+
+module.exports = libraryData;
